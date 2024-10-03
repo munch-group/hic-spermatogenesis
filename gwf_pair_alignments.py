f34a45b04ac7b73dfa7c62df97a6a1dd5e74c71b
sojernj <sojernj@gmail.com>
2024-10-03 22:21:49 +0200

    Mapping PE with bwa, pairtools, for use with cooler

--- gwf_pair_alignments.py
+++ gwf_pair_alignments.py
@@ -10,7 +10,7 @@
 # %%
 from gwf import *
 import glob
-import os
+import os.path as op
 
 #######################################
 #
@@ -36,12 +36,12 @@ gwf = Workflow(defaults={'nodes': 1, 'queue':"normal", 'account':"hic-spermatoge
 ############### Templates ###################
 #############################################
 
-def pair_sort_alignments(bam_merged, sorted_pairs):
+def pair_sort_alignments(chromsizes, bam_merged, sorted_pairs):
     """Pair the merged alignments from mate-pair sequencing with `pairtools parse`"""
     inputs = [bam_merged]
     outputs = [f"{bam_merged}_parsed.stats", 
                sorted_pairs]
-    options = {'cores':12, 'memory':"4g", 'walltime':"01:00:00"}
+    options = {'cores':12, 'memory':"4g", 'walltime':"02:00:00"}
     spec=f"""
 source $(conda info --base)/etc/profile.d/conda.sh
 conda activate hic
@@ -51,12 +51,50 @@ pairtools parse \
     --output-stats {bam_merged}_parsed.stats \
     --add-columns mapq \
     --assembly rheMac10 --no-flip \
-    --walks-policy mask 
+    --walks-policy mask \
     {bam_merged} | \
 pairtools sort -o {sorted_pairs} 
 """
     return AnonymousTarget(inputs=inputs, outputs=outputs, options=options, spec=spec)
 
+def dedup(sorted_pairs, ):
+    """Deduplicate the sorted pairs with `pairtools dedup`"""
+    pairs_prefix = sorted_pairs.split(".sorted")[0]
+    inputs = [sorted_pairs]
+    outputs = [f"{pairs_prefix}.nodups.pairs.gz",
+               f"{pairs_prefix}.nodups.bam",
+               f"{pairs_prefix}.unmapped.pairs.gz",
+               f"{pairs_prefix}.unmapped.bam",
+               f"{pairs_prefix}.dups.pairs.gz",
+               f"{pairs_prefix}.dups.bam",
+               f"{pairs_prefix}.dedup.stats"]
+    options = {'cores':12, 'memory': "4g", 'walltime': "01:00:00"}
+    spec = f"""
+source $(conda info --base)/etc/profile.d/conda.sh
+conda activate hic
+pairtools dedup \
+    --max-mismatch 3 \
+    --mark-dups \
+    --output \
+        >(pairtools split \
+            --output-pairs {pairs_prefix}.nodups.pairs.gz \
+            --output-sam {pairs_prefix}.nodups.bam \
+         ) \
+    --output-unmapped \
+        >( pairtools split \
+            --output-pairs {pairs_prefix}.unmapped.pairs.gz \
+            --output-sam {pairs_prefix}.unmapped.bam \
+         ) \
+    --output-dups \
+        >( pairtools split \
+            --output-pairs {pairs_prefix}.dups.pairs.gz \
+            --output-sam {pairs_prefix}.dups.bam \
+            ) \
+    --output-stats {pairs_prefix}.dedup.stats \
+    {sorted_pairs}
+
+    """
+    return AnonymousTarget(inputs=inputs, outputs=outputs, options=options, spec=spec)
 
 
 #############################################
@@ -64,8 +102,24 @@ pairtools sort -o {sorted_pairs}
 #############################################
 
 # Define the chromsizes file
-chromsizes = "data/links/ucsc_ref/misc/rheMac10.chrom.sizes.gz"
+chromsizes = "data/links/ucsc_ref/misc/rheMac10.chrom.sizes"
 
 # Find the merged bam files
 merged_bams = gwf.glob("steps/bowtie2/local/bamfiles/paired/*_paired.bam")
 
+sorted_bams = sorted(merged_bams)
+
+
+for inbam in sorted_bams:
+    base = op.basename(inbam)
+    prefix = base.split("_paired")[0]
+    
+    outdir = "steps/bowtie2/local/bamfiles/paired/"
+    outfile = f"{base.split("_paired")[0]}" + ".sorted.pairs.gz"
+    out_sorted_pairs = op.join(outdir, outfile)
+
+    gwf.target_from_template(f"pair_sort_{prefix}",
+                            pair_sort_alignments(chromsizes, inbam, out_sorted_pairs))
+    
+    gwf.target_from_template(f"dedup_{prefix}",
+                            dedup(out_sorted_pairs))
